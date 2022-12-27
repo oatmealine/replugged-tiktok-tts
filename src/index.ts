10c42c30f1a67116ac1fdd30e7fe7f5bd2b4fa9b
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2022-12-27 06:35:23 +0300

    init asterisk (see #1)

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,19 @@
-import { Injector, webpack } from "replugged";
+import { Injector, webpack as _webpack } from "replugged";
 
 const inject = new Injector();
 
 export async function start(): Promise<void> {
-  const typingMod = await webpack.waitForModule<{
-    startTyping: (channelId: string) => void;
-  }>(webpack.filters.byProps("startTyping"));
-  const getChannelMod = await webpack.waitForModule<{
-    getChannel: (id: string) => {
-      name: string;
-    };
-  }>(webpack.filters.byProps("getChannel"));
+  /*
+  const contextMenu = await webpack.waitForModule<{
+    openContextMenu: (test: string) => void;
+  }>(webpack.filters.byProps('openContextMenu'));
 
-  if (typingMod && getChannelMod) {
-    inject.instead(typingMod, "startTyping", ([channel]) => {
-      const channelObj = getChannelMod.getChannel(channel);
-      console.log(`Typing prevented! Channel: #${channelObj?.name ?? "unknown"} (${channel}).`);
+  if (contextMenu) {
+    inject.after(contextMenu, 'openContextMenu', (test) => {
+      console.log(test);
     });
   }
+  */
 }
 
 export function stop(): void {
